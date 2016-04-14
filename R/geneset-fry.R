0131ced6e36de033f81c4a59d6f56e03ee6c313d
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-14 09:01:21 +0000

    14 April 2006: limma 3.27.17
    - Improve capabilities and improved power for fry().  fry() has two
      new arguments, 'geneid' and 'standardize'.  The index argument of
      fry() can now be a list of data.frames containing identifiers and
      weights for each set.  The options introduced by the standardize
      argument allows fry() to be more powerful when genes have unequal
      variances.  fry() now accepts any arguments that would be suitable
      for lmFit() or eBayes().
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@116263 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/geneset-fry.R
+++ R/geneset-fry.R
@@ -1,128 +1,128 @@
 fry <- function(y,...) UseMethod("fry")
 
-fry.default <- function(y,index=NULL,design=NULL,contrast=ncol(design),weights=NULL,sort="directional",...)
+fry.default <- function(y,index=NULL,design=NULL,contrast=ncol(design),sort="directional",geneid=NULL,standardize="posterior.sd",...)
 #	Quick version of roast gene set test assuming equal variances between genes
 #	The up and down p-values are equivalent to those from roast with nrot=Inf
 #	in the special case of prior.df=Inf.
 #	Gordon Smyth and Goknur Giner
-#	Created 30 January 2015.  Last modified 22 December 2015
+#	Created 30 January 2015.  Last modified 12 April 2016
 {
-#	Issue warning if extra arguments found
-	dots <- names(list(...))
-	if(length(dots)) warning("Extra arguments disregarded: ",sQuote(dots))
+#	Partial matching of extra arguments
+	Dots <- list(...)
+	PossibleArgs <- c("array.weights","weights","block","correlation","trend","robust","winsor.tail.p")
+	if(!is.null(names(Dots))) {
+		i <- pmatch(names(Dots),PossibleArgs)
+		names(Dots) <- PossibleArgs[i]
+	}
+	if(is.null(Dots$trend)) Dots$trend <- FALSE
+	if(is.null(Dots$robust)) Dots$robust <- FALSE
 
-#	Extract components from y
-	y <- getEAWP(y)
-	G <- nrow(y$exprs)
-	n <- ncol(y$exprs)
+#	Trended eBayes
+	covariate <- NULL
+	if(Dots$trend) covariate <- rowMeans(as.matrix(y))
+
+#	Compute effects matrix
+	Effects <- .lmEffects(y=y,design=design,contrast=contrast,
+		array.weights=Dots$array.weights,
+		weights=Dots$weights,
+		block=Dots$block,
+		correlation=Dots$correlation)
+
+#	Divide out genewise standard deviations
+	standardize <- match.arg(standardize, c("none","residual.sd","posterior.sd"))
+	if(!standardize=="none") {
+
+#		Estimate genewise sds robustly
+		OK <- requireNamespace("statmod",quietly=TRUE)
+		if(!OK) stop("statmod package required but not available")
+		gq <- statmod::gauss.quad.prob(128,"uniform")
+		df.residual <- ncol(Effects)-1
+		Eu2max <- sum( (df.residual+1)*gq$nodes^df.residual*qchisq(gq$nodes,df=1)*gq$weights )
+		u2max <- apply(Effects^2,1,max)
+		s2.robust <- (rowSums(Effects^2)-u2max) / (df.residual+1-Eu2max)
+
+#		Empirical Bayes moderation
+		if(standardize=="posterior.sd") {
+			sv <- squeezeVar(s2.robust, df=0.92*df.residual,
+				covariate=covariate,
+				robust=Dots$robust,
+				winsor.tail.p=Dots$winsor.tail.p)
+			s2.robust <- sv$var.post
+		}
+
+		Effects <- Effects/sqrt(s2.robust)
+	}
+
+#	Check geneid (used if index contains data.frames)
+	if(is.null(geneid)) {
+		geneid <- rownames(Effects)
+	} else {
+		geneid <- as.character(geneid)
+		if(length(geneid)==1) 
+			geneid <- as.character(y$genes[,geneid])
+		else
+			if(length(geneid) != nrow(y)) stop("geneid vector should be of length nrow(y)")
+	}
+
+	.fryEffects(effects=Effects,index=index,geneid=geneid,sort=sort)
+}
+
+
+.fryEffects <- function(effects,index=NULL,geneid=rownames(effects),sort=TRUE)
+#	fry given the effects matrix
+#	Gordon Smyth and Goknur Giner
+#	Created 30 January 2015.  Last modified 12 April 2016
+{
+	G <- nrow(effects)
+	neffects <- ncol(effects)
+	df.residual <- neffects-1
 
 #	Check index
 	if(is.null(index)) index <- list(set1=1L:G)
-	if(!is.list(index)) index <- list(set1=index)
-	nsets <- length(index)
-	if(nsets==0) stop("index is empty")
-
-#	Check design
-	if(is.null(design)) design <- y$design
-	if(is.null(design))
-		stop("design matrix not specified")
-	else {
-		design <- as.matrix(design)
-		if(mode(design) != "numeric") stop("design must be a numeric matrix")
-	}
-	if(nrow(design) != n) stop("row dimension of design matrix must match column dimension of data")
-	ne <- nonEstimable(design)
-	if(!is.null(ne)) cat("Coefficients not estimable:",paste(ne,collapse=" "),"\n")
-
-	p <- ncol(design)
-	df.residual <- n-p
-
-#	Check weights
-	if(is.null(weights)) weights <- y$weights
-
-#	Reduce to numeric expression matrix
-	y <- y$exprs
-
-#	Check weights
-	if(!is.null(weights)) {
-		if(any(weights<=0)) stop("weights must be positive")
-		if(length(weights)==n) {
-			sw <- sqrt(weights)
-			y <- t(t(y)*sw)
-			design <- design*sw
-			weights <- NULL
-		}
-	}
-	if(!is.null(weights)) {
-		if(length(weights)==G) weights <- matrix(weights,G,n)
-		weights <- as.matrix(weights)
-		if(any( dim(weights) != dim(y) )) stop("weights not conformal with y")
-	}
-
-#	Reform design matrix so that contrast of interest is last column
-	if(is.character(contrast)) {
-		contrast <- which(contrast==colnames(design))
-		if(length(contrast)==0) stop("coef ",contrast," not found")
-	}
-	if(length(contrast)==1) {
-		j <- c((1:p)[-contrast], contrast)
-		if(contrast<p) design <- design[,j]
-	} else {
-		QR <- qr(contrast)
-		design <- t(qr.qty(QR,t(design)))
-		if(sign(QR$qr[1,1]<0)) design[,1] <- -design[,1]
-		design <- design[,c(2:p,1)]
-	}
-
-#	Compute effects matrix
-	if(is.null(weights)) {
-		QR <- qr(design)
-		if(QR$rank<p) stop("design matrix is not of full rank")
-		effects <- qr.qty(QR,t(y))
-		unscaledt <- effects[p,]
-		if(QR$qr[p,p]<0) unscaledt <- -unscaledt
-	} else {
-		effects <- matrix(0,n,G)
-		unscaledt <- rep(0,n)
-		sw <- sqrt(weights)
-		yw <- y*sw
-		for (g in 1:G) {
-			xw <- design*sw[g,]
-			QR <- qr(xw)
-			if(QR$rank<p) stop("weighted design matrix not of full rank for gene ",g)
-			effects[,g] <- qr.qty(QR,yw[g,])
-			unscaledt[g] <- effects[p,g]
-			if(QR$qr[p,p]<0) unscaledt[g] <- -unscaledt[g]
-		}
-	}
-
-#	Standardized residuals
-	U <- t(effects[-(1:(p-1)),,drop=FALSE])
+	if(is.data.frame(index) || !is.list(index)) index <- list(set1=index)
 
 #	Global statistics
 	nsets <- length(index)
+	if(nsets==0) stop("index is empty")
 	NGenes <- rep.int(0L,nsets)
-	Direction <- rep.int("",nsets)
-	PValue.Mixed <- PValue <- rep.int(0,nsets)
+	PValue.Mixed <- t.stat <- rep.int(0,nsets)
 	for (i in 1:nsets) {
 		iset <- index[[i]]
-		USet <- U[iset,,drop=FALSE]
-		NGenes[i] <- nrow(USet)
-		MeanUSet <- colMeans(USet)
-		t.stat <- MeanUSet[1L] / sqrt(mean(MeanUSet[-1L]^2L))
-		if(t.stat>0) Direction[i] <- "Up" else Direction[i] <- "Down"
-		PValue[i] <- 2*pt(-abs(t.stat),df=df.residual)
-
-		if(NGenes[i]==1) {
-			PValue.Mixed[i] <- PValue[i]
+		if(is.data.frame(iset)) {
+			if(ncol(iset)>1 && is.numeric(iset[,2])) {
+				iw <- iset[,2]
+				iset <- iset[,1]
+				if(is.factor(iset)) iset <- as.character(iset)
+				if(is.character(iset)) {
+					if(anyDuplicated(iset)) stop("Duplicate gene ids in set ",i)
+					j <- match(geneid,iset)
+					inset <- which(!is.na(j))
+					EffectsSet <- effects[inset,,drop=FALSE]
+					iw <- iw[j[inset]]
+				} else {
+					EffectsSet <- effects[iset,,drop=FALSE]
+				}
+				MeanEffectsSet <- iw %*% EffectsSet
+			} else {
+				stop("index ",i," is a data.frame but doesn't contain gene weights")
+			}
 		} else {
-			SVD <- svd(USet,nu=0)
+			if(is.factor(iset)) iset <- as.character(iset)
+			if(is.character(iset)) iset <- which(geneid %in% iset)
+			EffectsSet <- effects[iset,,drop=FALSE]
+			MeanEffectsSet <- colMeans(EffectsSet)
+		}
+		t.stat[i] <- MeanEffectsSet[1] / sqrt(mean(MeanEffectsSet[-1]^2))
+		NGenes[i] <- nrow(EffectsSet)
+
+		if(NGenes[i]>1) {
+			SVD <- svd(EffectsSet,nu=0)
 			A <- SVD$d^2
 			d1 <- length(A)
 			d <- d1-1L
 			beta.mean <- 1/d1
 			beta.var <- d/d1/d1/(d1/2+1)
-			Fobs <- (sum(USet[,1]^2)-A[d1]) / (A[1]-A[d1])
+			Fobs <- (sum(EffectsSet[,1]^2)-A[d1]) / (A[1]-A[d1])
 			Frb.mean <- (sum(A) * beta.mean - A[d1]) / (A[1]-A[d1])
 			COV <- matrix(-beta.var/d,d1,d1)
 			diag(COV) <- beta.var
@@ -133,6 +133,11 @@ fry.default <- function(y,index=NULL,design=NULL,contrast=ncol(design),weights=N
 			PValue.Mixed[i] <- pbeta(Fobs,shape1=alpha,shape2=beta,lower.tail=FALSE)
 		}
 	}
+
+	Direction <- rep.int("Up",nsets)
+	Direction[t.stat<0] <- "Down"
+	PValue <- 2*pt(-abs(t.stat),df=df.residual)
+	PValue.Mixed[NGenes==1] <- PValue[NGenes==1]
 
 #	Add FDR
 	if(nsets>1) {
